2f37feb0c9baead9ab90b655027d14eaba54d9f6
gus07ven <gustavoarismendi@hotmail.com>
2017-03-05 21:39:49 -0600

    Initial web app commit

---
+++ ContosoUniversity/ContosoUniversity/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)] // Lets you enter the primary key for the course rather than having the database generate it.
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
